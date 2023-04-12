a23a4e7990198d0b61b35187924c3fb107925cd4
LiTengko <litengko@gmail.com>
2023-04-12 21:22:00 +0800

    更改ts数据表为tau，并生成了ts表

--- data_generate.py
+++ data_generate.py
@@ -38,6 +38,9 @@ B_NUM = 30
 Cb = 35
 R_MAX = 20
 
+output_folder = "./data"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 def generate_time_slots(N):
     """
@@ -101,7 +104,7 @@ def generate_Nv(N):
     return result
 
 
-def generate_ts_matrix(size):
+def generate_tau_matrix(size):
     """
     生成游览时间矩阵，对应位置(i,j)对应到tau_ij
     这里生成的为10~30的随机数
@@ -110,15 +113,137 @@ def generate_ts_matrix(size):
     """
     matrix = np.random.randint(10, 31, (size, size))
     # 上三角转置合并后形成对角矩阵
-    ts_matrix = np.triu(matrix) + np.triu(matrix, 1).T
+    tau_matrix = np.triu(matrix) + np.triu(matrix, 1).T
     # 主对角线元素为0
-    np.fill_diagonal(ts_matrix, 0)
-    return ts_matrix
+    np.fill_diagonal(tau_matrix, 0)
+    return tau_matrix
 
 
-output_folder = "./data"
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+
+
+
+def time_period(time):
+    """
+    判断时间属于哪个时间段
+    :param time: 时间格式变量
+    :return: 时间段1，2，3
+    """
+    if time >= pd.Timestamp('1900-01-01 07:00:00') and time <= pd.Timestamp('1900-01-01 10:00:00'):
+        return 1
+    elif time > pd.Timestamp('1900-01-01 10:00:00') and time <= pd.Timestamp('1900-01-01 15:00:00'):
+        return 2
+    elif time > pd.Timestamp('1900-01-01 15:00:00') and time <= pd.Timestamp('1900-01-01 17:00:00'):
+        return 3
+    else:
+        return -1
+
+
+def ts_select(p_list):
+    """
+    根据输入的p_list生成均值不同的高斯分布
+    :param p_list: 景点对应的序列
+    :return: 返回的表示时间的list
+    """
+    ts_list = []
+    # 定义字典, 对应不同景区游览时间的均值
+    mean_values = {
+        1: 40,
+        2: 60,
+        3: 70,
+        4: 50,
+        5: 90,
+        6: 120,
+        7: 90
+    }
+
+    for value in p_list:
+        if value == -1:
+            ts_list.append(-1)
+        else:
+            # 生成高斯分布整数并添加到新列表B
+            gaussian_value = int(np.random.normal(mean_values[value], 10))
+            ts_list.append(gaussian_value)
+
+    return ts_list
+
+
+def guss_int_generate(min_num, max_num):
+    """
+    以高斯分布形式生成[min,max]之间的随机数
+    :param min:
+    :param max:
+    :return: int
+    """
+    mean = (min_num + max_num)/2  # 选择均值，使得整数范围在min到max之间
+    std_dev = 0.85  # 标准差
+
+    random_float = np.random.normal(loc=mean, scale=std_dev)
+    random_integer = int(round(random_float))
+
+    # 确保生成的整数在min到max之间
+    random_integer = max(min(random_integer, max_num), min_num)
+
+    return random_integer
+
+
+def ts_generate(csv_file):
+    """
+    根据已有的csv_file生成游客在景区的停留时间
+    并创建 ts.csv
+    :param csv_file: visitor.csv 的文件路径
+    :return: None
+    """
+    if os.path.exists(csv_file):
+        # 读取CSV文件
+        data = pd.read_csv(csv_file)
+        # 将"TE"列转换为时间格式
+        data['TE'] = pd.to_datetime(data['TE'], format="%H:%M")
+
+        new_data = pd.DataFrame(columns=['ID', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'])
+        new_rows = []
+        for index, row in data.iterrows():
+            # 创建一个与景点数，所有元素初始化为-1
+            row_list = [-1] * P_NUM
+
+            # 读取"TE"列的数据
+            time = row['TE']
+
+            # 判断时间属于哪个时间段
+            period = time_period(time)
+
+            if period == 1:
+                num_positions = guss_int_generate(4, 7)
+            elif period == 2:
+                num_positions = guss_int_generate(2, 4)
+            elif period == 3:
+                num_positions = guss_int_generate(1, 3)
+            else:
+                num_positions = 0
+
+            # 随机选择列进行标记
+            positions = random.sample(range(P_NUM), num_positions)
+            for position in positions:
+                row_list[position] = position + 1
+
+            p_list = ts_select(row_list)
+            # 添加新行到new_data DataFrame
+            new_row = pd.DataFrame({
+                'ID': [int(index) + 1],
+                'P1': [p_list[0]],
+                'P2': [p_list[1]],
+                'P3': [p_list[2]],
+                'P4': [p_list[3]],
+                'P5': [p_list[4]],
+                'P6': [p_list[5]],
+                'P7': [p_list[6]]
+            })
+
+            new_rows.append(new_row)
+        new_data = pd.concat(new_rows, ignore_index=True)
+        output_file = os.path.join(output_folder, "ts.csv")
+        new_data.to_csv(output_file, index=False)
+    else:
+        print("请检查是否已经生成visitor.csv")
 
 """
 # visitor 数据表生成
@@ -135,12 +260,15 @@ output_file = os.path.join(output_folder, "visitor.csv")
 visitor_data.to_csv(output_file, index=False)
 """
 
-# ts 数据表生成
+"""
+# tau 数据表生成
 # 包含有大门，因此最终矩阵规模为 P_NUM + 1
-ts_matrix = generate_ts_matrix(P_NUM + 1)
-# print(ts_matrix)
-ts_data = pd.DataFrame(ts_matrix)
-ts_data.columns = ['g', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7']
-output_file = os.path.join(output_folder, "ts.csv")
-ts_data.to_csv(output_file, index=False)
+tau_matrix = generate_tau_matrix(P_NUM + 1)
+# print(tau_matrix)
+tau_data = pd.DataFrame(tau_matrix)
+tau_data.columns = ['g', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7']
+output_file = os.path.join(output_folder, "tau.csv")
+tau_data.to_csv(output_file, index=False)
+"""
 
+ts_generate('./data/visitor.csv')
