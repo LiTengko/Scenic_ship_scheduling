16c09415455e5ba52b00d1f81a245adb874c0a1b
LiTengko <litengko@gmail.com>
2023-04-12 13:48:25 +0800

    Initial commit

---
+++ data_generate.py
@@ -0,0 +1,20 @@
+# _*_ coding:utf-8 _*_
+"""
+@File     : data_generate.py
+@Project  : MIP_solver
+@Time     : 2023/4/12 13:13
+@Author   : Li D.K
+@Contact  : lidengke@zju.edu.cn
+@Software : PyCharm
+@License  : (C)Copyright 2022 SRTP
+@Modify Time      @Author    @Version    @Description
+------------      -------    --------    -----------
+2023/4/12 13:13   Li D.K.      1.0         None
+-----------------------------------------------------
+Feature description：
+Generate corresponding data
+store the data in excel format in the directory ./data
+and refer to the documentation for the method of generating data.
+"""
+
+# import lib
