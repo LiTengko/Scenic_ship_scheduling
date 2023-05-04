f6c412456b1bf300afb4ed054d2f3b7bd08da230
LiTengko <litengko@gmail.com>
2023-05-04 19:48:15 +0800

    完成了粗略统计部分

--- ts_model.py
+++ ts_model.py
@@ -126,17 +126,18 @@ def rough_value(X, type = None):
     wait_total = 0  # 总等待时间
 
     # 对上述行程进行规划分配
-    while all(elem[5] != -1 for elem in x_tour):  # 当有行程没有被规划时，执行以下
+    while any(elem[-1] == -1 for elem in x_tour):  # 当有行程没有被规划时，执行以下
         # 找到行程未分配中最早的团
         v_index = 0
-        early_time = min([x_tour[i][3] for i in range(len(x_tour)) if x_tour[i][3] == -1])
+        early_time = min([x_tour[i][3] for i in range(len(x_tour)) if x_tour[i][5] == -1])
         for i in range(len(x_tour)):
             if x_tour[i][5] == -1 and x_tour[i][3] == early_time:
                 v_index = i
 
         # 获得该团的信息
+        v_id = x_tour[v_index][0]  #编号
         v_i = x_tour[v_index][1]  # 出发点
-        v_j = x_tour[v_index][1]  # 终点
+        v_j = x_tour[v_index][2]  # 终点
         v_z_GD = x_tour[v_index][3]  # 预期出发时间
         # 最终找到的船编号为b_id, 终点为b_j
         b_id = 1
@@ -150,7 +151,7 @@ def rough_value(X, type = None):
             b_num += 1
             b_id = b_num
             b_j = 0
-            b_list = [b_num, v_i, v_j, v_z_GD, v_z_GD]  # [b_i, i, j, z^BS, z^BF]  此时视驳船出发为到达大门时间
+            b_list = [b_num, v_i, v_j, v_z_GD, v_z_GD]  # [b_i, i, j, z^BS, z^BF]  此时视驳船出发为到达大门时间 （后续视成团情况对z^BS, z^BF更新）
             B.append(b_list)
         else:  # 集合中查找
 
@@ -193,11 +194,13 @@ def rough_value(X, type = None):
             # 判断是否要加入空程
             if add_flag == 1:
                 add_flag = 0
-                b_list = [b_id, 0, v_i, v_z_GD - tau[0, v_i], v_z_GD]  # [b_i, i, j, z^BS, z^BF]  此时视驳船出发为到达大门时间
-                B.append(b_list)
+                if v_i != 0:
+                    b_list = [b_id, 0, v_i, v_z_GD - tau[0, v_i], v_z_GD]  # [b_i, i, j, z^BS, z^BF]  此时视驳船出发为到达大门时间
+                    B.append(b_list)
             else:
                 if v_i != b_j:
                     b_list = [b_id, b_j, v_i, b_z_BF, b_z_BF + tau[v_i, b_j]]  # [b_i, i, j, z^BS, z^BF]
+                    B.append(b_list)
 
         # 驳船安排好后，考虑是否两者打包一起出游
 
@@ -209,6 +212,122 @@ def rough_value(X, type = None):
 
         if same_trip:
             sorted_same_trip = sorted(same_trip, key=lambda x: x[3])
+            # 初始化驳船容量
+            C = Nv[v_id]
+            z_GD_temp = v_z_GD
+            same_index = 0
+            for trip in sorted_same_trip:
+                if C + Nv[trip[0]] > model_index.Cb:
+                    # 超出容量退出
+                    break
+                else:
+                    cost1 = 0 + Nv[v_id]/C * model_index.c3 * tau[v_i, v_j]  # 0 额外等待成本 + 均摊行驶成本
+                    C_temp = C + Nv[trip[0]]
+                    cost2 = abs(z_GD_temp - trip[3]) * model_index.c4 + model_index.c3 * Nv[v_id]/C_temp * tau[v_i, v_j] # 额外等待成本 + 均摊行驶成本
+                    if cost1 < cost2:
+                        # 退出，不加同行
+                        break
+                    else:
+                        # 添加一个同行
+                        same_index += 1
+                        z_GD_temp = max(z_GD_temp, trip[3])
+                        C = C_temp
+            # sorted_same_trip 前 index 同行
+            if same_index == 0:
+                # 没有同行的团，仅考虑单独出行
+                # 没有同行的团，则仅考虑单独出行
+                wait_total += abs(B[-1][4] + tau[b_j, v_i] - v_z_GD)  # 索引最后一个元素
+                # 实际出发时间
+                v_z_GD = max(B[-1][4] + tau[b_j, v_i], v_z_GD)
+                # 实际到达时间
+                v_z_GA = v_z_GD + tau[v_i, v_j]
+                # 出发前往下一景点时间
+                if v_j != 0:
+                    v_z_GD_next = v_z_GA + ts[v_id, v_j]
+                # 在写入之前加入对是否超时返回的判断
+                # 如果超时，标记该行程，并删除所有未被标记的行程，并添加该点到景区大门的行程
+                if v_z_GA > model_index.TE and v_j != 0:
+                    # 对该游团的行程进行写入
+                    for index in range(len(x_tour)):
+                        if x_tour[index][0] == v_id and x_tour[index][1] == v_i:
+                            x_tour[index][3] = v_z_GD
+                            x_tour[index][4] = v_z_GA
+                            x_tour[index][5] = 1
+                    # 删除所有还没有被安排的行程
+                    x_tour = [x for x in x_tour if x[-1] != -1]
+                    # 加入从该景点返回的行程
+                    x_tour.append([v_index, v_j, 0, v_z_GD, v_z_GA, -1])
+                else:
+                    # 对该游团的行程进行写入
+                    for index in range(len(x_tour)):
+                        if x_tour[index][0] == v_id  and x_tour[index][1] == v_i:
+                            x_tour[index][3] = v_z_GD
+                            x_tour[index][4] = v_z_GA
+                            x_tour[index][5] = 1
+                        if x_tour[index][0] == v_id and x_tour[index][1] == v_j and v_j != 0:
+                            x_tour[index][3] = v_z_GD_next
+                # 对驳船行程进行写入
+                B.append([b_id, v_i, v_j, v_z_GD, v_z_GA])
+            else:
+                # 出发时间为最大时间
+                v_z_GD_max = sorted_same_trip[same_index - 1][3]
+                max_z_v = max(v_z_GD_max, B[-1][4] + tau[b_j, v_i])
+                wait_total += abs(max_z_v - v_z_GD)
+                # 实际出发时间
+                v_z_GD = max_z_v
+                # 实际到达时间
+                v_z_GA = v_z_GD + tau[v_i, v_j]
+                # 出发前往下一景点时间
+                if v_j != 0:
+                    v_z_GD_next = v_z_GA + ts[v_id, v_j]
+                # 如果超时，标记该行程，并删除所有未被标记的行程，并添加该点到景区大门的行程
+                if v_z_GA > model_index.TE and v_j != 0:
+                    # 对该游团的行程进行写入
+                    for index in range(len(x_tour)):
+                        if x_tour[index][0] == v_id  and x_tour[index][1] == v_i:
+                            x_tour[index][3] = v_z_GD
+                            x_tour[index][4] = v_z_GA
+                            x_tour[index][5] = 1
+                    # 删除所有还没有被安排的行程
+                    x_tour = [x for x in x_tour if x[-1] != -1]
+                    # 加入从该景点返回的行程
+                    x_tour.append([v_index, v_j, 0, v_z_GD, v_z_GA, -1])
+                else:
+                    # 对该游团的行程进行写入
+                    for index in range(len(x_tour)):
+                        if x_tour[index][0] == v_id  and x_tour[index][1] == v_i:
+                            x_tour[index][3] = v_z_GD
+                            x_tour[index][4] = v_z_GA
+                            x_tour[index][5] = 1
+                        if x_tour[index][0] == v_id  and x_tour[index][1] == v_j and v_j != 0:
+                            x_tour[index][3] = v_z_GD_next
+                # 对下属的同行景点进行写入
+                for ii in range(same_index):
+                    v_ii = sorted_same_trip[ii][0]
+                    v_j = sorted_same_trip[ii][2]
+                    # 如果超时，标记该行程，并删除所有未被标记的行程，并添加该点到景区大门的行程
+                    if v_z_GA > model_index.TE and v_j != 0:
+                        # 对该游团的行程进行写入
+                        for index in range(len(x_tour)):
+                            if x_tour[index][0] == v_ii and x_tour[index][1] == v_i:
+                                x_tour[index][4] = v_z_GA
+                                x_tour[index][5] = 1
+                        # 删除所有还没有被安排的行程
+                        x_tour = [x for x in x_tour if x[-1] != -1]
+                        # 加入从该景点返回的行程
+                        x_tour.append([v_index, v_j, 0, v_z_GD, v_z_GA, -1])
+                    else:
+                        # 对该游团的行程进行写入
+                        for index in range(len(x_tour)):
+                            if x_tour[index][0] == v_id and x_tour[index][1] == v_i:
+                                x_tour[index][3] = v_z_GD
+                                x_tour[index][4] = v_z_GA
+                                x_tour[index][5] = 1
+                            if x_tour[index][0] == v_id and x_tour[index][1] == v_j and v_j != 0:
+                                x_tour[index][3] = v_z_GD_next
+
+                # 对驳船行程进行写入
+                B.append([b_id, v_i, v_j, v_z_GD, v_z_GA])
 
         else:
             # 没有同行的团，则仅考虑单独出行
@@ -217,18 +336,48 @@ def rough_value(X, type = None):
             v_z_GD = max(B[-1][4] + tau[b_j, v_i], v_z_GD)
             # 实际到达时间
             v_z_GA = v_z_GD + tau[v_i, v_j]
-            v_z_GD_next = v_z_GA + ts[v_index, v_j]
+            # 出发前往下一景点时间
+            if v_j != 0:
+                v_z_GD_next = v_z_GA + ts[v_id, v_j]
             # 在写入之前加入对是否超时返回的判断
-
+            # 如果超时，标记该行程，并删除所有未被标记的行程，并添加该点到景区大门的行程
+            if v_z_GA > model_index.TE and v_j != 0:
+                # 对该游团的行程进行写入
+                for index in range(len(x_tour)):
+                    if x_tour[index][0] == v_index and x_tour[index][1] == v_i:
+                        x_tour[index][4] = v_z_GA
+                        x_tour[index][5] = 1
+                # 删除所有还没有被安排的行程
+                x_tour = [x for x in x_tour if x[-1] != -1]
+                # 加入从该景点返回的行程
+                x_tour.append([v_index, v_j, 0, v_z_GD, v_z_GA, -1])
+            else:
             # 对该游团的行程进行写入
-            for index in range(len(x_tour)):
-                if x_tour[index][0] == v_index and x_tour[index][1] == v_i:
-                    x_tour[index][4] = v_z_GA
-                    x_tour[index][5] = 1
-                if x_tour[index][0] == v_index and x_tour[index][1] == v_j:
-                    x_tour[index][3] = v_z_GD_next
+                for index in range(len(x_tour)):
+                    if x_tour[index][0] == v_index and x_tour[index][1] == v_i:
+                        x_tour[index][4] = v_z_GA
+                        x_tour[index][5] = 1
+                    if x_tour[index][0] == v_index and x_tour[index][1] == v_j and v_j != 0:
+                        x_tour[index][3] = v_z_GD_next
             # 对驳船行程进行写入
             B.append([b_id, v_i, v_j, v_z_GD, v_z_GA])
+
+    # 计算整体成本
+    wait_cost = model_index.c4 * wait_total
+    tour_cost = 0
+    price_income = 0
+    N_total = sum(Nv[key] for key in X.keys())
+    for b_ii in B:
+        tour_cost += model_index.c3 * tau[b_ii[1], b_ii[2]]
+    fix_cost = model_index.c2 * b_num
+    if type == 1:
+        return model_index.c1 * N_total * model_index.P_all_a - (wait_cost + tour_cost + fix_cost)
+    elif type == 2:
+        for x_ii in x_tour:
+            if x_ii[2] != 0:
+                price_income += model_index.c1 * Nv[x_ii[0]] * model_index.pm[x_ii[2]]
+        return model_index.c1 * N_total * model_index.P_all_b + price_income - (wait_cost + tour_cost + fix_cost)
+
 
 
 
@@ -238,5 +387,9 @@ for v_i in range(1, model_index.V_NUM + 1):
     X[v_i] = TSP_optimize(v_i)
 
 print(X)
-x_tour = X_split(X)
-print(x_tour)
+# x_tour = X_split(X)
+# print(x_tour)
+
+cost = rough_value(X, type=2)
+
+print(cost)
